4397f39cfef513baf4391a7c113b8f021f7a5948
Otavio Rodolfo Piske <opiske@redhat.com>
2021-09-23 10:24:06 +0200

    Fix not running on 3.12 due to the removed consumerStreams configuration

--- start-consumer.sh
+++ start-consumer.sh
@@ -2,4 +2,10 @@ CAMEL_VERSION=${2:-"3.12.0-SNAPSHOT"}
 CONSUMER_MESSAGE_COUNT=${3:-1000000000}
 CONSUMER_COUNT=${4:-1}
 
-mvn exec:java -Dcamel.version=$CAMEL_VERSION -Dcamel.main.durationMaxMessages=$CONSUMER_MESSAGE_COUNT -Dcamel.component.kafka.consumer-streams=$CONSUMER_COUNT -Dcamel.component.kafka.consumers-count=$CONSUMER_COUNT -Dtest.file=$CAMEL_VERSION.test -Dcamel.component.kafka.brokers=$1 -Dexec.mainClass="org.apache.camel.kafka.tester.MainConsumer"
+if [[ $CAMEL_VERSION == "3.11.2" ]] ; then
+	mvn exec:java -Dcamel.version=$CAMEL_VERSION -Dcamel.main.durationMaxMessages=$CONSUMER_MESSAGE_COUNT -Dcamel.component.kafka.consumer-streams=$CONSUMER_COUNT -Dcamel.component.kafka.consumers-count=$CONSUMER_COUNT -Dtest.file=$CAMEL_VERSION.test -Dcamel.component.kafka.brokers=$1 -Dexec.mainClass="org.apache.camel.kafka.tester.MainConsumer"
+else
+	mvn exec:java -Dcamel.version=$CAMEL_VERSION -Dcamel.main.durationMaxMessages=$CONSUMER_MESSAGE_COUNT -Dcamel.component.kafka.consumers-count=$CONSUMER_COUNT -Dtest.file=$CAMEL_VERSION.test -Dcamel.component.kafka.brokers=$1 -Dexec.mainClass="org.apache.camel.kafka.tester.MainConsumer"
+fi
+
+
