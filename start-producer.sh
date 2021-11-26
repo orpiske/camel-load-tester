789f006bf738065fd2dd97d7d60e5524bf150aa5
Otavio Rodolfo Piske <opiske@redhat.com>
2021-11-26 15:15:59 +0100

    Use the Camel version in the name to make it simple to know which version the jar is testing

--- start-producer.sh
+++ start-producer.sh
@@ -1,5 +1,4 @@
 CAMEL_VERSION=${2:-"3.12.0-SNAPSHOT"}
 PRODUCER_MESSAGE_COUNT=${3:-1000000000}
-PROJECT_VERSION=1.0.0-SNAPSHOT
 
-java -jar -Dcamel.version=$CAMEL_VERSION -Dcamel.main.durationMaxMessages=$PRODUCER_MESSAGE_COUNT -Dtest.file=$CAMEL_VERSION.test -Dcamel.component.kafka.brokers=$1 target/kafka-tester-${PROJECT_VERSION}-producer.jar
+java -jar -Dcamel.version=$CAMEL_VERSION -Dcamel.main.durationMaxMessages=$PRODUCER_MESSAGE_COUNT -Dtest.file=$CAMEL_VERSION.test -Dcamel.component.kafka.brokers=$1 target/kafka-tester-${CAMEL_VERSION}-producer.jar
