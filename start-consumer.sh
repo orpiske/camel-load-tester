71a791e72f99cf77961a68f2f8f6e91cffc362dc
Otavio Rodolfo Piske <opiske@redhat.com>
2021-11-26 15:13:03 +0100

    Converted to use the shade to make it easier to test this in different hosts

--- start-consumer.sh
+++ start-consumer.sh
@@ -1,11 +1,12 @@
 CAMEL_VERSION=${2:-"3.12.0-SNAPSHOT"}
 CONSUMER_MESSAGE_COUNT=${3:-1000000000}
 CONSUMER_COUNT=${4:-1}
+PROJECT_VERSION=1.0.0-SNAPSHOT
 
 if [[ $CAMEL_VERSION == "3.11.2" ]] ; then
-	mvn exec:java -Dcamel.version=$CAMEL_VERSION -Dcamel.main.durationMaxMessages=$CONSUMER_MESSAGE_COUNT -Dcamel.component.kafka.consumer-streams=$CONSUMER_COUNT -Dcamel.component.kafka.consumers-count=$CONSUMER_COUNT -Dtest.file=$CAMEL_VERSION.test -Dcamel.component.kafka.brokers=$1 -Dexec.mainClass="org.apache.camel.kafka.tester.MainConsumer"
+	java -jar -Dcamel.version=$CAMEL_VERSION -Dcamel.main.durationMaxMessages=$CONSUMER_MESSAGE_COUNT -Dcamel.component.kafka.consumer-streams=$CONSUMER_COUNT -Dcamel.component.kafka.consumers-count=$CONSUMER_COUNT -Dtest.file=$CAMEL_VERSION.test -Dcamel.component.kafka.brokers=$1 target/kafka-tester-${PROJECT_VERSION}-consumer.jar
 else
-	mvn exec:java -Dcamel.version=$CAMEL_VERSION -Dcamel.main.durationMaxMessages=$CONSUMER_MESSAGE_COUNT -Dcamel.component.kafka.consumers-count=$CONSUMER_COUNT -Dtest.file=$CAMEL_VERSION.test -Dcamel.component.kafka.brokers=$1 -Dexec.mainClass="org.apache.camel.kafka.tester.MainConsumer"
+	java -jar -Dcamel.version=$CAMEL_VERSION -Dcamel.main.durationMaxMessages=$CONSUMER_MESSAGE_COUNT -Dcamel.component.kafka.consumers-count=$CONSUMER_COUNT -Dtest.file=$CAMEL_VERSION.test -Dcamel.component.kafka.brokers=$1 target/kafka-tester-${PROJECT_VERSION}-consumer.jar
 fi
 
 
--- start-producer.sh
+++ start-producer.sh
@@ -1,5 +1,5 @@
 CAMEL_VERSION=${2:-"3.12.0-SNAPSHOT"}
 PRODUCER_MESSAGE_COUNT=${3:-1000000000}
+PROJECT_VERSION=1.0.0-SNAPSHOT
 
-# mvn exec:java -Dcamel.version=$CAMEL_VERSION -Dcamel.component.kafka.brokers=$1 -Dexec.mainClass="org.apache.camel.kafka.tester.MainProducer"
-mvn exec:java -Dcamel.version=$CAMEL_VERSION -Dcamel.main.durationMaxMessages=$PRODUCER_MESSAGE_COUNT -Dtest.file=$CAMEL_VERSION.test -Dcamel.component.kafka.brokers=$1 -Dexec.mainClass="org.apache.camel.kafka.tester.MainProducer"
+java -jar -Dcamel.version=$CAMEL_VERSION -Dcamel.main.durationMaxMessages=$PRODUCER_MESSAGE_COUNT -Dtest.file=$CAMEL_VERSION.test -Dcamel.component.kafka.brokers=$1 target/kafka-tester-${PROJECT_VERSION}-producer.jar
