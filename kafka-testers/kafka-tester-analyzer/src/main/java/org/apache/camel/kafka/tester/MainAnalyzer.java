a67331a1c98b75e306d1d060b41ffda022428188
Otavio Rodolfo Piske <opiske@redhat.com>
2021-12-16 11:51:04 +0100

    Improved reporting

--- kafka-testers/kafka-tester-analyzer/src/main/java/org/apache/camel/kafka/tester/MainAnalyzer.java
+++ kafka-testers/kafka-tester-analyzer/src/main/java/org/apache/camel/kafka/tester/MainAnalyzer.java
@@ -96,14 +96,31 @@ public class MainAnalyzer {
 
 
     private static void analyze(Histogram histogram, Histogram baseline) {
+        LOG.info("LATENCY INFO:");
         LOG.info("Latency start time: {}", histogram.getStartTimeStamp());
         LOG.info("Latency end time: {}", histogram.getEndTimeStamp());
         LOG.info("Latency max value: {}", histogram.getMaxValue());
+
         LOG.info("p50 (median) latency: {} / baseline: {}", histogram.getValueAtPercentile(50.0), baseline.getValueAtPercentile(50.0));
+        double p50Delta = histogram.getValueAtPercentile(50.0) - baseline.getValueAtPercentile(50.0);
+        logDeltas(p50Delta, "p50 (median) latency");
+
         LOG.info("p90 latency: {} / baseline: {}", histogram.getValueAtPercentile(90.0), baseline.getValueAtPercentile(90.0));
+
+        double p90Delta = histogram.getValueAtPercentile(90.0) - baseline.getValueAtPercentile(90.0);
+        logDeltas(p90Delta, "p90 latency");
+
         LOG.info("p95 latency: {} / baseline: {}", histogram.getValueAtPercentile(95.0), baseline.getValueAtPercentile(95.0));
+        double p95Delta = histogram.getValueAtPercentile(95.0) - baseline.getValueAtPercentile(95.0);
+        logDeltas(p95Delta, "p95 latency");
+
         LOG.info("p99 latency: {} / baseline: {}", histogram.getValueAtPercentile(99.0), baseline.getValueAtPercentile(99.0));
+        double p99Delta = histogram.getValueAtPercentile(99.0) - baseline.getValueAtPercentile(99.0);
+        logDeltas(p99Delta, "p99 latency");
+
         LOG.info("p99.9 latency: {} / baseline: {}", histogram.getValueAtPercentile(99.9), baseline.getValueAtPercentile(99.9));
+        double p999Delta = histogram.getValueAtPercentile(99.9) - baseline.getValueAtPercentile(99.9);
+        logDeltas(p999Delta, "p99.9 latency");
     }
 
     private static void plot(RateData testData) throws IOException {
@@ -155,17 +172,63 @@ public class MainAnalyzer {
         SummaryStatistics testStatistics = getStats(testData);
         SummaryStatistics baselineStatistics = getStats(baselineData);
 
+        LOG.info("TEST INFO:");
         LOG.info("Test suite version: {}", testData.header.getCamelVersion());
         LOG.info("Version: {}", testData.header.getFileVersion());
         LOG.info("Type: {}", testData.header.getRole().name());
 
-        LOG.info("Test total: {} | Baseline total: {}", testStatistics.getSum(), baselineStatistics.getSum());
-        LOG.info("Test minimum: {} | Baseline minimum: {}", testStatistics.getMin(), baselineStatistics.getMin());
-        LOG.info("Test maximum: {} | Baseline maximum: {}", testStatistics.getMax(), baselineStatistics.getMax());
-        LOG.info("Test mean: {} | Baseline mean: {}", testStatistics.getMean(), baselineStatistics.getMean());
-        LOG.info("Test geometric mean: {} | Baseline geometric mean: {}", testStatistics.getGeometricMean(),
-                baselineStatistics.getGeometricMean());
+        LOG.info("Test version: {} / Baseline version: {}", testData.header.getCamelVersion(), baselineData.header.getCamelVersion());
+
+        LOG.info("");
+        LOG.info("TOTAL EXCHANGES:");
+        LOG.info("Test: {} | Baseline: {}", testStatistics.getSum(), baselineStatistics.getSum());
+
+        double totalDelta = testStatistics.getSum() - baselineStatistics.getSum();
+        LOG.info("Delta: {}", totalDelta);
+        logDeltas(totalDelta, "total number of exchanges");
+
+        LOG.info("");
+        LOG.info("MINIMUM RATE");
+        LOG.info("Test: {} | Baseline: {}", testStatistics.getMin(), baselineStatistics.getMin());
+        final double minDelta = testStatistics.getMin() - baselineStatistics.getMin();
+        LOG.info("Delta: {}", minDelta);
+        logDeltas(minDelta, "minimum rate");
+
+        LOG.info("");
+        LOG.info("MAXIMUM RATE:");
+        LOG.info("Test: {} | Baseline: {}", testStatistics.getMax(), baselineStatistics.getMax());
+        final double maxDelta = testStatistics.getMax() - baselineStatistics.getMax();
+        LOG.info("Delta: {}", maxDelta);
+        logDeltas(maxDelta, "maximum rate");
+
+        LOG.info("");
+        LOG.info("MEAN (RATE):");
+        LOG.info("Test: {} | Baseline: {}", testStatistics.getMean(), baselineStatistics.getMean());
+        final double meanDelta = testStatistics.getMean() - baselineStatistics.getMean();
+        LOG.info("Delta: {}", meanDelta);
+        logDeltas(meanDelta, "mean rate");
+
+        LOG.info("");
+        LOG.info("GEOMETRIC MEAN (RATE):");
+        LOG.info("Test: {} | Baseline: {}", testStatistics.getGeometricMean(), baselineStatistics.getGeometricMean());
+        final double geoMeanDelta = testStatistics.getGeometricMean() - baselineStatistics.getGeometricMean();
+        LOG.info("Delta: {}", geoMeanDelta);
+        logDeltas(geoMeanDelta, "geometric mean rate");
+
+        LOG.info("");
+        LOG.info("OTHER:");
         LOG.info("Test standard deviation: {} | Baseline standard deviation: {}", testStatistics.getStandardDeviation(), baselineStatistics.getStandardDeviation());
+
+        LOG.info("");
+        LOG.info("SUMMARY:");
+    }
+
+    private static void logDeltas(double delta, String name) {
+        if (delta < 0) {
+            LOG.warn("The {} was lower for the test than for the baseline", name);
+        } else {
+            LOG.info("The {} was greater for the test than for the baseline", name);
+        }
     }
 
 
