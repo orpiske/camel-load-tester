9912aade2257f75912838b1660e244f8b3fdbcfd
Otavio R. Piske <angusyoung@gmail.com>
2022-02-17 20:43:23 +0100

    Multiple script fixes

--- scripts/runner/analyzer/analyze-consumer.sh
+++ scripts/runner/analyzer/analyze-consumer.sh
@@ -1,16 +1,18 @@
 TEST_NAME=$1
 BASELINE_VERSION=$2
 TEST_VERSION=$3
-DATA_DIR=$4
+DATA_DIR=${4:-${HOME}/tmp/kafka-tester-data}
 
-ANALYZER_HOME=/home/opiske/code/test/kafka-tester/kafka-testers/kafka-tester-analyzer/target
-ANALYZER_PATH=${ANALYZER_HOME}/kafka-tester-analyzer-3.14.0-SNAPSHOT.jar
+ANALYZER_HOME=../../../kafka-testers/kafka-tester-analyzer
+ANALYZER_PATH=${ANALYZER_HOME}/target/kafka-tester-analyzer-3.*.jar
+
+JAR_FILE=$(ls -1 ${ANALYZER_PATH})
 
 java -Dbaseline.rate.file=${DATA_DIR}/consumer-rate-${TEST_NAME}-${BASELINE_VERSION}.data \
 	-Dbaseline.latencies.file=${DATA_DIR}/consumer-latencies-${TEST_NAME}-${BASELINE_VERSION}.hdr \
 	-Dtest.rate.file=${DATA_DIR}/consumer-rate-${TEST_NAME}-${TEST_VERSION}.data \
 	-Dtest.name=${TEST_NAME} \
-	-jar ${ANALYZER_PATH}
+	-jar ${JAR_FILE}
 
 mkdir ${TEST_NAME}-consumer-report
 mv consumer_rate.png ${TEST_NAME}-consumer-report
