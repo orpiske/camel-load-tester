7bda77c1be8d9efc645953356d64c009f889417c
Otavio Rodolfo Piske <angusyoung@gmail.com>
2022-11-18 10:47:34 +0100

    Code cleanup

--- kafka-testers/kafka-tester-producer/src/main/java/org/apache/camel/kafka/tester/MainProducer.java
+++ kafka-testers/kafka-tester-producer/src/main/java/org/apache/camel/kafka/tester/MainProducer.java
@@ -1,11 +1,14 @@
 package org.apache.camel.kafka.tester;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.LongAdder;
 
 import org.HdrHistogram.EncodableHistogram;
 import org.HdrHistogram.SingleWriterRecorder;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.dataset.ListDataSet;
 import org.apache.camel.component.dataset.SimpleDataSet;
 import org.apache.camel.kafka.tester.common.TestMainListener;
 import org.apache.camel.kafka.tester.io.common.FileHeader;
@@ -25,7 +28,7 @@ public class MainProducer {
     public static void main(String... args) throws Exception {
         Main main = new Main();
 
-        final String topic = System.getProperty("kafka.topic", "test-topic-producer");
+
         final String testRateFileName = System.getProperty("test.rate.file", "producer-rate.data");
 
         final LongAdder longAdder = new LongAdder();
@@ -33,23 +36,51 @@ public class MainProducer {
 
         bindDataSet(main, testSize);
 
-        int batchSize = Integer.parseInt(System.getProperty("test.batch.size", "0"));
-
-        SingleWriterRecorder latencyRecorder = new SingleWriterRecorder(TimeUnit.HOURS.toMicros(1), 3);
-
         File testRateFile = new File(testRateFileName);
         try (RateWriter rateWriter = new BinaryRateWriter(testRateFile, FileHeader.WRITER_DEFAULT_PRODUCER)) {
-            if (batchSize > 0) {
-                main.configure().addRoutesBuilder(new TestNoopProducer(latencyRecorder, longAdder, true, batchSize, topic));
-            } else {
-                main.configure().addRoutesBuilder(new TestNoopProducer(latencyRecorder, longAdder, false, 0, topic));
-            }
-
+            RouteBuilder routeBuilder = getRouteBuilder(longAdder);
+            main.configure().addRoutesBuilder(routeBuilder);
             main.addMainListener(new TestMainListener(rateWriter, longAdder, testSize, main::stop));
 
             main.run();
         }
+    }
 
+    private static RouteBuilder getRouteBuilder(LongAdder longAdder) {
+        String routeType = System.getProperty("test.producer.type", "kafka");
+
+        switch (routeType) {
+            case "kafka": return getKafkaRouteBuilder(longAdder);
+            case "noop": return getTestNoopProducer(longAdder);
+        }
+
+        throw new IllegalArgumentException("Invalid route type: " + routeType);
+    }
+
+    private static TestNoopProducer getTestNoopProducer(LongAdder longAdder) {
+        int batchSize = Integer.parseInt(System.getProperty("test.batch.size", "0"));
+        if (batchSize > 0) {
+            return new TestNoopProducer(longAdder, true, batchSize);
+        } else {
+            return new TestNoopProducer(longAdder, false, batchSize);
+        }
+    }
+
+    private static KafkaProducerRouteBuilder getKafkaRouteBuilder(LongAdder longAdder) {
+        final String topic = System.getProperty("kafka.topic", "test-topic-producer");
+        int batchSize = Integer.parseInt(System.getProperty("test.batch.size", "0"));
+        SingleWriterRecorder latencyRecorder = new SingleWriterRecorder(TimeUnit.HOURS.toMicros(1), 3);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> saveLatencyFile(latencyRecorder)));
+
+        if (batchSize > 0) {
+            return new KafkaProducerRouteBuilder(latencyRecorder, longAdder, true, batchSize, topic);
+        } else {
+            return new KafkaProducerRouteBuilder(latencyRecorder, longAdder, false, batchSize, topic);
+        }
+    }
+
+    private static void saveLatencyFile(SingleWriterRecorder latencyRecorder) {
         final String testLatenciesFileName = System.getProperty("test.latencies.file", "producer-latencies.hdr");
         try (LatencyWriter latencyWriter = new LatencyWriter(new File(testLatenciesFileName))) {
             EncodableHistogram histogram = latencyRecorder.getIntervalHistogram();
@@ -57,13 +88,16 @@ public class MainProducer {
             String camelVersion = System.getProperty("camel.version", "3.x.x");
             histogram.setTag(camelVersion);
             latencyWriter.outputIntervalHistogram(histogram);
+        } catch (IOException e) {
+            System.err.println("Unable to save latency file: " + e.getMessage());
+            throw new RuntimeException(e);
         }
     }
 
     private static void bindDataSet(Main main, int testSize) {
         SimpleDataSet simpleDataSet = new SimpleDataSet();
 
-        simpleDataSet.setDefaultBody("test");
+        simpleDataSet.setDefaultBody(Boolean.TRUE);
         simpleDataSet.setSize(testSize);
 
         main.bind("testSet", simpleDataSet);
--- kafka-testers/kafka-tester-producer/src/main/java/org/apache/camel/kafka/tester/TestNoopProducer.java
+++ kafka-testers/kafka-tester-producer/src/main/java/org/apache/camel/kafka/tester/TestNoopProducer.java
@@ -1,12 +1,8 @@
 package org.apache.camel.kafka.tester;
 
-import java.time.Duration;
-import java.time.Instant;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.LongAdder;
 
 import org.HdrHistogram.SingleWriterRecorder;
-import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.processor.aggregate.GroupedExchangeAggregationStrategy;
 import org.slf4j.Logger;
@@ -19,18 +15,14 @@ import org.slf4j.LoggerFactory;
 public class TestNoopProducer extends RouteBuilder {
     private static final Logger LOG = LoggerFactory.getLogger(TestNoopProducer.class);
 
-    private final SingleWriterRecorder latencyRecorder;
     private final LongAdder longAdder;
     private final boolean aggregate;
     private final int batchSize;
-    private final String topic;
 
-    public TestNoopProducer(SingleWriterRecorder latencyRecorder, LongAdder longAdder, boolean aggregate, int batchSize, String topic) {
-        this.latencyRecorder = latencyRecorder;
+    public TestNoopProducer(LongAdder longAdder, boolean aggregate, int batchSize) {
         this.longAdder = longAdder;
         this.aggregate = aggregate;
         this.batchSize = batchSize;
-        this.topic = topic;
     }
 
     /**
@@ -39,28 +31,12 @@ public class TestNoopProducer extends RouteBuilder {
     public void configure() {
         if (!aggregate) {
             from("dataset:testSet?produceDelay=0&minRate={{?min.rate}}&initialDelay={{initial.delay:2000}}")
-                    .setProperty("CREATE_TIME", Instant::now)
-                    .process(exchange -> longAdder.increment())
-                    .process(this::measureExchange);
+                    .process(exchange -> longAdder.increment());
         } else {
             from("dataset:testSet?produceDelay=0&initialDelay={{initial.delay:2000}}&minRate={{?min.rate}}&preloadSize={{?preload.size}}")
                     .aggregate(constant(true), new GroupedExchangeAggregationStrategy())
                     .completionSize(batchSize)
-                    .setProperty("CREATE_TIME", Instant::now)
-                    .process(exchange -> longAdder.add(batchSize))
-                    .process(this::measureExchange);
+                    .process(exchange -> longAdder.add(batchSize));
         }
     }
-
-    private void measureExchange(Exchange exchange) {
-        Instant sent = exchange.getProperty("CREATE_TIME", Instant.class);
-        if (sent != null) {
-            Duration duration = Duration.between(sent, Instant.now());
-            latencyRecorder.recordValue(TimeUnit.NANOSECONDS.toMicros(duration.toNanos()));
-        } else {
-            LOG.warn("Skipping latency processing for exchange due to missing CREATE_TIME property");
-        }
-
-    }
-
 }
