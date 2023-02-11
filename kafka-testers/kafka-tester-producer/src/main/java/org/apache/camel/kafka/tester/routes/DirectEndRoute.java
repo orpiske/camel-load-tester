3535b6211f5d262bf94993595e5d51accb9fe18f
Otavio Rodolfo Piske <angusyoung@gmail.com>
2023-02-11 19:03:31 +0100

    Do log the number of consumer and processing threads

--- kafka-testers/kafka-tester-producer/src/main/java/org/apache/camel/kafka/tester/routes/DirectEndRoute.java
+++ kafka-testers/kafka-tester-producer/src/main/java/org/apache/camel/kafka/tester/routes/DirectEndRoute.java
@@ -5,8 +5,12 @@ import java.util.concurrent.atomic.LongAdder;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.kafka.tester.common.Counter;
 import org.apache.camel.kafka.tester.common.Parameters;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DirectEndRoute extends RouteBuilder {
+    private static final Logger LOG = LoggerFactory.getLogger(DirectEndRoute.class);
+
     private final int threadCount;
     private final LongAdder longAdder;
 
@@ -17,6 +21,8 @@ public class DirectEndRoute extends RouteBuilder {
 
     @Override
     public void configure() {
+        LOG.info("Using thread count for parallel processing: {}", threadCount);
+
         if (threadCount == 0) {
             from("direct:test")
                     .routeId("noop-to-direct")
--- kafka-testers/kafka-tester-producer/src/main/java/org/apache/camel/kafka/tester/routes/SedaEndRoute.java
+++ kafka-testers/kafka-tester-producer/src/main/java/org/apache/camel/kafka/tester/routes/SedaEndRoute.java
@@ -4,8 +4,11 @@ import java.util.concurrent.atomic.LongAdder;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.kafka.tester.common.Counter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SedaEndRoute extends RouteBuilder {
+    private static final Logger LOG = LoggerFactory.getLogger(SedaEndRoute.class);
     private final int threadCount;
     private final LongAdder longAdder;
 
@@ -16,6 +19,8 @@ public class SedaEndRoute extends RouteBuilder {
 
     @Override
     public void configure() {
+        LOG.info("Using thread count for parallel consumption: {}", threadCount);
+
         if (threadCount == 0) {
             from("seda:test")
                     .routeId("noop-to-seda")
