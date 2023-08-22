24f11b9b2cc39068fe955a4ee1d9da8e375a553c
Otavio Rodolfo Piske <angusyoung@gmail.com>
2023-08-22 07:58:41 +0200

    Removed deprecated scripts

--- scripts/runner/controller/start-controller.sh
+++
@@ -1,8 +0,0 @@
-TOOLS_HOME=${HOME}/tools
-install_path=$(dirname $0)
-
-source  ${install_path}/config/launcher.conf
-
-java -Dconfig.dir=${TESTER_DIR}/config/ \
-  -javaagent:${TOOLS_HOME}/prometheus/jmx_prometheus_javaagent.jar=0.0.0.0:9404:${install_path}/config/prometheus/config.yaml \
-  -jar ${TESTER_DIR}/controller-test-${CONTROLLER_VERSION}*.jar
--- scripts/runner/controller/start-session.sh
+++
@@ -1,6 +0,0 @@
-TOOLS_HOME=${HOME}/tools
-install_path=$(dirname $0)
-
-source  ${install_path}/config/launcher.conf
-
-tmux new-session -d -s "controller" ${TESTER_DIR}/start-controller.sh
